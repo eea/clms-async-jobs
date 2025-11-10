f7baec8a63a5850af11ed4b67c3948fcc48392c0
GhitaB <ghitabizau27@gmail.com>
2025-11-10 16:21:56 +0200

    Refs #293054 - Use types.

--- src/jobs.ts
+++ src/jobs.ts
@@ -1,19 +1,23 @@
-import fetch from 'node-fetch';
+import type { CreateCDSEBatches, TypeJobsMapping } from "./types";
+import mockData from "./mock-data.json";
+import { RateLimitError } from "bullmq";
 
-const PLONE_URL = process.env.PLONE_URL || 'http://localhost:8080/Plone';
-const PLONE_AUTH_TOKEN = process.env.PLONE_AUTH_TOKEN || '';
+import fetch from "node-fetch";
+
+const PLONE_URL = process.env.PLONE_URL || "http://localhost:8080/Plone";
+const PLONE_AUTH_TOKEN = process.env.PLONE_AUTH_TOKEN || "hello1234";
 
 /**
  * Call Plone's @@start-cdse-batch view with provided data.
  */
-export async function create_cdse_batches(data: any) {
+export async function create_cdse_batches(data: CreateCDSEBatches) {
   const url = `${PLONE_URL}/@@start-cdse-batch`;
   console.log(`[Worker] Calling ${url} ...`);
 
   const res = await fetch(url, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       Authentication: PLONE_AUTH_TOKEN, // security token check
     },
     body: JSON.stringify(data),
@@ -25,7 +29,7 @@ export async function create_cdse_batches(data: any) {
   }
 
   const json = await res.json();
-  console.log('[Worker] Received response:', json);
+  console.log("[Worker] Received response:", json);
 
   return json;
 }
@@ -33,6 +37,6 @@ export async function create_cdse_batches(data: any) {
 /**
  * Mapping between job names and handler functions.
  */
-export const JOBS_MAPPING: Record<string, Function> = {
+export const JOBS_MAPPING: TypeJobsMapping = {
   create_cdse_batches,
 };
